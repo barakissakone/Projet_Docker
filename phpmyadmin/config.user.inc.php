b2994b74b4e95774e6bd0d795696ff44967fac93
Kone Barakissa <barakissakone412@gmail.com>
2026-01-21 20:37:33 +0100

    config

--- phpmyadmin/config.user.inc.php
+++ phpmyadmin/config.user.inc.php
@@ -1,10 +1,8 @@
 <?php
 $i = 1;
-
 $cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['host'] = getenv('PMA_HOST') ?: 'db';
-$cfg['Servers'][$i]['port'] = getenv('PMA_PORT') ?: '3306';
-$cfg['Servers'][$i]['user'] = getenv('PMA_USER') ?: 'rdv_user';
-$cfg['Servers'][$i]['password'] = getenv('PMA_PASSWORD') ?: '';
-
+$cfg['Servers'][$i]['host'] = 'db';
+$cfg['Servers'][$i]['user'] = 'rdv_user';
+$cfg['Servers'][$i]['password'] = 'rdv_password';
+$cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
